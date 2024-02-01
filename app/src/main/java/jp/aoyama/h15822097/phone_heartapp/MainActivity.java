39caefc065582f1d0ab01c49e29e02fe60641c14
hamo <63552479+Hamo203@users.noreply.github.com>
2024-02-01 16:45:15 +0900

    Initial commit

---
+++ app/src/main/java/jp/aoyama/h15822097/phone_heartapp/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.aoyama.h15822097.phone_heartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
