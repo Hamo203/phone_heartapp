241fc1734fc6addec148016d29cf953a22a86574
hamo <63552479+Hamo203@users.noreply.github.com>
2024-04-11 15:36:19 +0900

    TabViewにボタンが見えるようにした

--- app/src/main/java/jp/aoyama/h15822097/phone_heartapp/Page1.java
+++ app/src/main/java/jp/aoyama/h15822097/phone_heartapp/Page1.java
@@ -1,31 +1,24 @@
 package jp.aoyama.h15822097.phone_heartapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
-import android.text.Layout;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 public class Page1 extends Fragment {
-
-    public Page1(){
+    public Page1() {
     }
-
-    public static Page1 newInstance(){
-        Page1 fragment=new Page1();
+    public static Page1 newInstance() {
+        Page1 fragment = new Page1();
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page1,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_page1, container, false);
     }
 }
--- app/src/main/java/jp/aoyama/h15822097/phone_heartapp/TapPagerAdapter.java
+++ app/src/main/java/jp/aoyama/h15822097/phone_heartapp/TapPagerAdapter.java
@@ -3,7 +3,6 @@ package jp.aoyama.h15822097.phone_heartapp;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-
 public class TapPagerAdapter extends FragmentStateAdapter {
     public TapPagerAdapter(MainTabActivity fragment) {
         super(fragment);
@@ -11,19 +10,17 @@ public class TapPagerAdapter extends FragmentStateAdapter {
     /**
      * 指定されたタブの位置(position) に対応するタブページ（Fragment）を作成する
      */
-
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        Fragment fragment=null;
-        if(position==0){
-            fragment=new Page1();
-        }else if(position==1){
-            fragment=new Page2();
+        Fragment fragment = null;
+        if (position == 0) {
+            fragment = new Page1();
+        } else if (position == 1) {
+            fragment = new Page2();
         }
         return fragment;
     }
-
     /**
      * タブの数を返す
      */
