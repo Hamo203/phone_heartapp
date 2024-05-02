07787c6f8326766b9c70e25ba3de0ec3f0bba710
hamo <63552479+Hamo203@users.noreply.github.com>
2024-05-02 17:03:04 +0900

    体重、年齢、安静時心拍数の入力欄を増やした

--- app/src/main/java/jp/aoyama/h15822097/phone_heartapp/MainTabActivity.java
+++ app/src/main/java/jp/aoyama/h15822097/phone_heartapp/MainTabActivity.java
@@ -5,11 +5,15 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainTabActivity extends AppCompatActivity {
     Button funcBtn;
@@ -35,6 +39,12 @@ public class MainTabActivity extends AppCompatActivity {
     }
     public void funcOnClick(View view){
         //センサー止める時に使う
+        // Dateオブジェクトを用いて現在時刻を取得してくる値を 変数 date に格納
+        Date date = new Date();
+        // SimpleDateFormat をオブジェクト化し、任意のフォーマットを設定
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss");
+        sdf.format(date);
+        Log.d("test","date="+sdf.format(date));
 
     }
     public void backonClick(View view){
